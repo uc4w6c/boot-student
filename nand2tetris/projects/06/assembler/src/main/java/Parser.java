03d4dd5b73f08237cc3442ff4b2732e4d2e74046
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2020-04-14 08:51:12 +0900

    Parserモジュール作成中

--- nand2tetris/projects/06/assembler/src/main/java/Parser.java
+++ nand2tetris/projects/06/assembler/src/main/java/Parser.java
@@ -17,7 +17,7 @@ public class Parser {
             String line = null;
             while ((line = reader.readLine()) != null) {
                 if (line.isEmpty()) continue;
-                if (line.trim().substring(2).equals("//")) continue;
+                if (line.trim().substring(0, 2).equals("//")) continue;
                 asmList.add(line.trim());
             }
         } catch (IOException e) {
@@ -41,8 +41,52 @@ public class Parser {
 
     public CommandType commandType() {
         var asm = this.asmList.get(this.point);
-        if (asm.indexOf(";") > 0) return CommandType.C_COMMAND;
-        if (asm.substring(0).equals("(")) return CommandType.L_COMMAND;
-        return CommandType.A_COMMAND;
+        if (asm.substring(0, 1).equals("@")) return CommandType.A_COMMAND;
+        if (asm.substring(0, 1).equals("(")) return CommandType.L_COMMAND;
+        return CommandType.C_COMMAND;
+    }
+
+    public String symbol() {
+        if (this.commandType().equals(CommandType.A_COMMAND)) {
+            return this.asmList.get(this.point).substring(1);
+        }
+        if (this.commandType().equals(CommandType.L_COMMAND)) {
+            return this.asmList.get(this.point)
+                                .replace("", "(")
+                                .replace("", ")");
+        }
+        return null;
+    }
+
+    public String dest() {
+        // TODO: 実装すること
+        // this.asmList.get(this.point);
+        return null;
+    }
+
+    public String comp() {
+
+    }
+
+    public String jump() {
+        var asm = this.asmList.get(this.point);
+        var neemockPoint = asm.indexOf(";");
+        if (neemockPoint < 0) {
+            return null;
+        }
+        var jumpNeemock = asm.substring(neemockPoint + 1);
+
+        switch(jumpNeemock) {
+            case "JGT": return "001";
+            case "JEQ": return "010";
+            case "JGE": return "011";
+            case "JLT": return "100";
+            case "JNE": return "101";
+            case "JLE": return "110";
+            case "JMP": return "110";
+            default:
+        }
+        // ここには遷移しない
+        return null;
     }
 }
